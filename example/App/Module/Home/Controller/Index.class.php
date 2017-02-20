c07093d5ee07b7ee96ccdc7c99571e71f23bf30a
Huangsikai <Huangsikai@cguoguo.com>
2017-02-20 09:49:28 +0800

    例子

--- example/App/Module/Home/Controller/Index.class.php
+++ example/App/Module/Home/Controller/Index.class.php
@@ -14,20 +14,10 @@ use SPHPCore\Lib\Mvc\Controller;
 class Index extends Controller
 {
     public function index(){
-
-
-
         $this->forward('Index','detail');
-   
     }
 
     public function detail(){
-//        echo $this->request->getCurrentActionName();
-//        $this->assign('name',array('a','b','c'=>array(123)));
-//        $this->sss='hello';
-//        $this->setLayout('layout');
         $this->response->view();
-//        $a->setLayout('a');
-//        $a->setLayout('a');
     }
 }
